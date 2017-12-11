ef91d1ad60c1367ad228e9f5a702e47d80c73d26
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2017-12-11 15:22:45 +0100

    Add dind scripts

---
+++ install-dind.sh
@@ -0,0 +1,9 @@
+# FIXME: 192.168.0.0/16 causes problems with Travis(?)
+export POD_NETWORK_CIDR="10.244.0.0/16"
+
+# Start dind cluster
+cd "$HOME" 
+wget https://cdn.rawgit.com/Mirantis/kubeadm-dind-cluster/master/fixed/dind-cluster-v1.8.sh
+chmod +x dind-cluster-v1.8.sh
+export NUM_NODES=3
+"$HOME"/dind-cluster-v1.8.sh up
