c041534142b2ade1ef58fdeb2be8a5291bf6fd8d
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-04-05 12:15:09 +0200

    Add fixed version of qserv container

--- k8s/env.example.sh
+++ k8s/env.example.sh
@@ -4,7 +4,7 @@
 
 # VERSION can be a git ticket branch but with _ instead of /
 # example: tickets_DM-7139, or dev
-VERSION=dev
+VERSION=1bef3d1
 MARIADB_VERSION=10.1.25
 # Version of the deplyment tool to use
 DEPLOY_VERSION=dev
