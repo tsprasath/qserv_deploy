145607013159afdac188aa36f6d9a61611526016
Benjamin Roziere <benjamin.roziere@clermont.in2p3.fr>
2018-04-18 11:36:06 +0200

    Cleanup indent

--- openstack/terraform/outputs.tf
+++ openstack/terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "gateway_external_ip" {
-	value = "${openstack_networking_floatingip_v2.floating_ip.address}"
+  value = "${openstack_networking_floatingip_v2.floating_ip.address}"
 }
 
 output "cluster_hosts" {
-	value = "${local.cluster_hosts}"
+  value = "${local.cluster_hosts}"
 }
