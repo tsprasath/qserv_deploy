142e01715613b7b2c728ee8c405e11d36a5f4dd2
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-07-06 18:07:27 +0200

    Add launch script for ci WIP

--- minikube-ci/launch.sh
+++ minikube-ci/launch.sh
@@ -1,2 +1,9 @@
 DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+export QSERV_CFG_DIR="$HOME/.qserv_deploy"
+mkdir -p "$QSERV_CFG_DIR"
+cp $HOME/.kube/config "$QSERV_CFG_DIR"/kubeconfig
+
+cp -r $DIR/../config.examples/minikube-ci/* "$QSERV_CFG_DIR"
+
 $DIR/../qserv-deploy.sh
