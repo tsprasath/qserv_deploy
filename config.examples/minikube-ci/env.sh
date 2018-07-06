e36c3a9626472d6b4e1532e4d8fe26e225fddecc
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-07-06 18:03:32 +0200

    Add minikube ci configuration WIP

---
+++ config.examples/minikube-ci/env.sh
@@ -0,0 +1,50 @@
+# Description: allow to customize pods execution
+#
+# Configuration file copied to orchestration node, in $ORCHESTRATION_DIR
+# and then sourced by Kubernetes ochestration node scripts
+
+# VERSION is relative to Qserv repository, it can be:
+#  - a git ticket branch but with _ instead of /
+#    example: tickets_DM-7139, or dev
+#  - a git hash
+VERSION=828ff67
+
+# `docker run` settings
+# =====================
+
+# Data directory location on docker host
+# HOST_DATA_DIR=/qserv/data
+
+# Log directory location on docker host
+HOST_LOG_DIR=/qserv/log
+
+# Qserv temporary directory location on docker host
+HOST_TMP_DIR=/qserv/tmp
+
+# Advanced configuration
+# ======================
+
+# CLUSTER_CONFIG_DIR is a global variable
+
+# FIXME: infrastructure should be abstracted from k8s
+# Parameters related to infrastructure,used to place containers:
+# - node hostnames
+. "$CLUSTER_CONFIG_DIR/env-infrastructure.sh"
+
+# Container image name
+CONTAINER_IMAGE="qserv/qserv:${VERSION}"
+
+# Pods names
+# ==========
+
+MASTER_POD='master'
+WORKER_POD_FORMAT='worker-%g'
+
+# List of worker pods (and containers) names
+j=1
+WORKER_PODS=''
+for host in $WORKERS;
+do
+    WORKER_PODS="$WORKER_PODS worker-$j"
+    j=$((j+1));
+done
