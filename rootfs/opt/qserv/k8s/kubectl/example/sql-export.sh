00c71abe78749aa2d024bb3a1bff83e07f549d4b
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-07-06 11:43:42 +0200

    Add sql export example script

---
+++ rootfs/opt/qserv/k8s/kubectl/example/sql-export.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Help removing qserv database 
+# WARN: do not currently remove database but only metadata 
+
+# @author Fabrice Jammes SLAC/IN2P3
+
+set -e
+set -x
+
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+CLUSTER_CONFIG_DIR="${CLUSTER_CONFIG_DIR:-/qserv-deploy/config}"
+. "$CLUSTER_CONFIG_DIR/env.sh"
+
+SQL="$1"
+# SQL="SELECT * FROM qservw_worker.Dbs"
+# SQL="USE qservw_worker; SHOW TABLES;"
+
+echo "Launch '$SQL' on all nodes"
+parallel --tag "kubectl exec {} -c mariadb -- \
+    bash -c \". /qserv/stack/loadLSST.bash && \
+    setup mariadbclient && \
+    mysql --socket /qserv/data/mysql/mysql.sock \
+    --user=root --password=changeme \
+    -e \\\"$SQL\\\"\"" ::: master worker-1
