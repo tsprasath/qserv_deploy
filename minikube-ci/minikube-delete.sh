4fc169fecfa25f0fbf64293303202b229583a20a
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-07-10 18:10:35 +0200

    Add minikube delete script

--- minikube-ci/minikube-delete.sh
+++
@@ -1,9 +0,0 @@
-set -e
-set -x
-
-sudo /usr/local/bin/minikube delete || echo "WARN: unable to delete minikube"
-sudo rm -rf /etc/kubernetes
-sudo rm -rf /root/.kube
-sudo rm -rf /root/.minikube
-rm -rf $HOME/.kube
-rm -rf $HOME/.minikube
