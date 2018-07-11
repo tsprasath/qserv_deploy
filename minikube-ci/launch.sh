cff473ae73284577fb842f5bef6aefadcd83187b
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-07-11 10:02:13 +0200

    Add multinode tests

--- minikube-ci/launch.sh
+++ minikube-ci/launch.sh
@@ -1,7 +1,6 @@
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 export MOUNT_DOT_MK=true
-export QSERV_DEV=true
 
 export QSERV_CFG_DIR="$HOME/.qserv_deploy"
 mkdir -p "$QSERV_CFG_DIR"
@@ -9,4 +8,5 @@ cp "$HOME"/.kube/config "$QSERV_CFG_DIR"/kubeconfig
 
 cp -r "$DIR"/../config.examples/minikube-ci/* "$QSERV_CFG_DIR"
 
-"$DIR"/../qserv-deploy.sh qserv-start
+"$DIR"/../qserv-deploy.sh /opt/qserv/bin/qserv-start
+"$DIR"/../qserv-deploy.sh /opt/qserv/k8s/kubectl/run-multinode-tests.sh 
