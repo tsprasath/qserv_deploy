b9dbb95856b0d1b3a06e9edfecafdda0bd68ebd8
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-07-12 12:01:06 +0200

    echo PATH

--- minikube-ci/minikube-create.sh
+++ minikube-ci/minikube-create.sh
@@ -1,16 +1,16 @@
 set -e
 set -x
 
-MINIKUBE_BIN=/usr/local/bin/minikube
-KUBECTL_BIN=/usr/local/bin/kubectl
+MINIKUBE_BIN="/usr/local/bin/minikube"
+KUBECTL_BIN="/usr/local/bin/kubectl"
 
-curl -Lo "$MINIKUBE_BIN" \
+sudo curl -Lo "$MINIKUBE_BIN" \
 https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-chmod +x "$MINIKUBE_BIN"
+sudo chmod +x "$MINIKUBE_BIN"
 
-curl -Lo "$KUBECTL_BIN" \
+sudo curl -Lo "$KUBECTL_BIN" \
 https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x "$KUBECTL_BIN"
+sudo chmod +x "$KUBECTL_BIN"
 
 export MINIKUBE_WANTUPDATENOTIFICATION=false
 export MINIKUBE_WANTREPORTERRORPROMPT=false
