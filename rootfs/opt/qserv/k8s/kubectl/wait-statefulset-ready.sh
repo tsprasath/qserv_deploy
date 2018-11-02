5a12ed404680120c409fa50d2bba3c83458572c3
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-11-02 18:55:58 +0100

    Improve output for pod waiter

--- rootfs/opt/qserv/k8s/kubectl/wait-statefulset-ready.sh
+++ rootfs/opt/qserv/k8s/kubectl/wait-statefulset-ready.sh
@@ -11,7 +11,7 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 CLUSTER_CONFIG_DIR="${CLUSTER_CONFIG_DIR:-/qserv-deploy/config}"
 . "$CLUSTER_CONFIG_DIR/env.sh"
 
-echo "Wait for Qserv statefulset to be in running state"
+echo "Wait for Qserv statefulsets to be in running state"
 
 GO_TPL="{{if .status.readyReplicas}}\
     .status.readyReplicas is set \
@@ -19,21 +19,25 @@ GO_TPL="{{if .status.readyReplicas}}\
 
 for sf in 'czar' 'qserv'
 do
+    echo -n "Wait for statefulset '$sf' to start first pod"
     until [ -n "$READY" ]
     do
-        echo "Wait for statefulset '$sf' to start first pod"
         READY=$(kubectl get statefulset "$sf" -o go-template --template "$GO_TPL")
-        sleep 1
+        sleep 2
+	echo -n '.'
     done
+    echo
 
+    echo -n "Wait for statefulset '$sf' to start all pods"
     GO_TPL="{{if and (eq .spec.replicas .status.replicas) \
         (eq .status.replicas .status.readyReplicas) \
         (eq .status.currentRevision .status.updateRevision)}}true{{end}}"
     until [ -n "$STARTED" ]
     do
-        echo "Wait for statefulset '$sf' to start all pods"
         STARTED=$(kubectl get statefulset "$sf" -o go-template --template "$GO_TPL")
         sleep 2
+	echo -n '.'
     done
+    echo
     echo "Statefulset '$sf' ready"
 done
